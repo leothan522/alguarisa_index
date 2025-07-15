4fbdf2427a1bc8affc6fbe3c5e4d1b1872ac1a72
Yonathan Castillo <leothan522@gmail.com>
2025-07-14 20:43:34 -0400

    Correcion spinner Auth

--- index.php
+++ index.php
@@ -220,7 +220,7 @@ function asset($uri = null, $noCache = false): string
                                 <img class="gobernacion_start d-lg-none" src="<?= asset('dist/img/logo_gobernacion.png')?>" alt="Logo Gobernación Guárico">
 
                                 <div class="text-center mt-5 <!-- pt-5-->">
-                                    <a href="<?= env('APP_URL', './')?>" onclick="verCargando()">
+                                    <a href="<?= env('APP_URL', './')?>" onclick="verCargandoAuth(this)">
                                         <img class="img-fluid mt-lg-5" src="<?= asset('dist/img/logo_alguarisa.png') ?>" alt="Logo Alguarisa">
                                     </a>
                                     <h6 class="mt-1 mb-4 pb-1 text_title"><strong>Dirección de Tecnología y Sistemas.</strong></h6>
@@ -231,7 +231,7 @@ function asset($uri = null, $noCache = false): string
                                     <div class="col-12">
 
                                         <div class="d-flex gap-3 mb-3 flex-column">
-                                            <a href="<?= env('URL_SISTEMA_ALMACEN', '#')?>" class="btn btn-lg btn-outline-dark" onclick="verCargando()">
+                                            <a href="<?= env('URL_SISTEMA_ALMACEN', '#')?>" class="btn btn-lg btn-outline-dark" onclick="verCargandoAuth(this)">
 
                                                 <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 24 24">
                                                     <path fill-rule="evenodd" d="M5.005 10.19a1 1 0 0 1 1 1v.233l5.998 3.464L18 11.423v-.232a1 1 0 1 1 2 0V12a1 1 0 0 1-.5.866l-6.997 4.042a1 1 0 0 1-1 0l-6.998-4.042a1 1 0 0 1-.5-.866v-.81a1 1 0 0 1 1-1ZM5 15.15a1 1 0 0 1 1 1v.232l5.997 3.464 5.998-3.464v-.232a1 1 0 1 1 2 0v.81a1 1 0 0 1-.5.865l-6.998 4.042a1 1 0 0 1-1 0L4.5 17.824a1 1 0 0 1-.5-.866v-.81a1 1 0 0 1 1-1Z" clip-rule="evenodd"/>
@@ -243,7 +243,7 @@ function asset($uri = null, $noCache = false): string
                                         </div>
 
                                         <div class="d-flex gap-3 flex-column mb-3">
-                                            <a href="<?= env('URL_SISTEMA_TRANSPORTE', '#') ?>" class="btn btn-lg btn-outline-dark" onclick="verCargando()">
+                                            <a href="<?= env('URL_SISTEMA_TRANSPORTE', '#') ?>" class="btn btn-lg btn-outline-dark" onclick="verCargandoAuth(this)">
 
                                                 <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 24 24">
                                                     <path fill-rule="evenodd" d="M4 4a2 2 0 0 0-2 2v9a1 1 0 0 0 1 1h.535a3.5 3.5 0 1 0 6.93 0h3.07a3.5 3.5 0 1 0 6.93 0H21a1 1 0 0 0 1-1v-4a.999.999 0 0 0-.106-.447l-2-4A1 1 0 0 0 19 6h-5a2 2 0 0 0-2-2H4Zm14.192 11.59.016.02a1.5 1.5 0 1 1-.016-.021Zm-10 0 .016.02a1.5 1.5 0 1 1-.016-.021Zm5.806-5.572v-2.02h4.396l1 2.02h-5.396Z" clip-rule="evenodd"/>
@@ -254,7 +254,7 @@ function asset($uri = null, $noCache = false): string
                                         </div>
 
                                         <div class="d-flex d-none gap-3 flex-column mb-3">
-                                            <a href="#" class="btn btn-lg btn-outline-dark" onclick="verCargando()">
+                                            <a href="#" class="btn btn-lg btn-outline-dark" onclick="verCargandoAuth(this)">
 
                                                 <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 24 24">
                                                     <path fill-rule="evenodd" d="M12 6a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7Zm-1.5 8a4 4 0 0 0-4 4 2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-3Zm6.82-3.096a5.51 5.51 0 0 0-2.797-6.293 3.5 3.5 0 1 1 2.796 6.292ZM19.5 18h.5a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-1.1a5.503 5.503 0 0 1-.471.762A5.998 5.998 0 0 1 19.5 18ZM4 7.5a3.5 3.5 0 0 1 5.477-2.889 5.5 5.5 0 0 0-2.796 6.293A3.501 3.501 0 0 1 4 7.5ZM7.1 12H6a4 4 0 0 0-4 4 2 2 0 0 0 2 2h.5a5.998 5.998 0 0 1 3.071-5.238A5.505 5.505 0 0 1 7.1 12Z" clip-rule="evenodd"/>
@@ -265,7 +265,7 @@ function asset($uri = null, $noCache = false): string
                                         </div>
 
                                         <div class="d-flex d-none gap-3 flex-column">
-                                            <a href="#" class="btn btn-lg btn-outline-dark" onclick="verCargando()">
+                                            <a href="#" class="btn btn-lg btn-outline-dark" onclick="verCargandoAuth(this)">
 
                                                 <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 24 24">
                                                     <path fill-rule="evenodd" d="M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H6Zm7.25-2.095c.478-.86.75-1.85.75-2.905a5.973 5.973 0 0 0-.75-2.906 4 4 0 1 1 0 5.811ZM15.466 20c.34-.588.535-1.271.535-2v-1a5.978 5.978 0 0 0-1.528-4H18a4 4 0 0 1 4 4v1a2 2 0 0 1-2 2h-4.535Z" clip-rule="evenodd"/>
@@ -281,6 +281,12 @@ function asset($uri = null, $noCache = false): string
                                             </div>
                                         </div>
 
+                                    </div>
+
+                                    <div class="position-absolute top-50 start-50 translate-middle d-none verCargando">
+                                        <div class="spinner-border text-primary" role="status">
+                                            <span class="visually-hidden">Loading...</span>
+                                        </div>
                                     </div>
 
                                 </div>
@@ -326,9 +332,20 @@ function asset($uri = null, $noCache = false): string
         })
     })()
 
-    function verCargando() {
-        document.querySelector("#card_body").classList.add('opacity-50');
-        document.querySelector(".verCargando").classList.remove('d-none');
+    function verCargandoAuth(enlace) {
+        event.preventDefault();
+        const card = document.querySelector("#card_body");
+        const spinner = document.querySelector(".verCargando");
+
+        card.classList.add('opacity-50');
+        spinner.classList.remove('d-none');
+
+        setTimeout(function () {
+            card.classList.remove('opacity-50');
+            spinner.classList.add('d-none');
+            //alert(enlace.href)
+            window.location.href = enlace.href;
+        }, 1000)
     }
 
     console.log('Hi!')
